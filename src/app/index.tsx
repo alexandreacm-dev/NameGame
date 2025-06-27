00886c28fb591ae7912448638f80ad476a1eed41
Alexandre Marques <alexandreacm.developer@gmail.com>
2025-06-27 11:05:33 -0300

    feat: Added all first files..

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function App() {
+  return (
+    <View>
+      <Text>APP</Text>
+    </View>
+  );
+}
+
+export default App;
